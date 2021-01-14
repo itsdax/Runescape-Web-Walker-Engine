73f1e90006a66b90df8891b676f3a137794931f7
FALSkills <crlange@mail.bradley.edu>
2021-01-14 17:00:53 -0600

    Fix Games necklace teleport tile

--- scripts/dax_api/teleports/Teleport.java
+++ scripts/dax_api/teleports/Teleport.java
@@ -240,7 +240,7 @@ public enum Teleport {
 	),
 
 	GAMES_NECK_CORPOREAL (
-		35, new RSTile(2965, 4832, 2),
+		35, new RSTile(2965, 4382, 2),
 		() -> inMembersWorld() && WearableItemTeleport.has(WearableItemTeleport.GAMES_NECKLACE_FILTER),
 		() -> WearableItemTeleport.teleport(WearableItemTeleport.GAMES_NECKLACE_FILTER, "(?i).*corpreal.*")
 	),
