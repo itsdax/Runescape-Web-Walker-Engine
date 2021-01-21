a24e8a60eec031cd0e0b4af6cc5fd5ae8571f6ff
FALSkills <crlange@mail.bradley.edu>
2021-01-21 10:46:56 -0600

    Add Watchtower setting + Dragon Slayer II varbit

--- scripts/dax_api/api_lib/models/PlayerDetails.java
+++ scripts/dax_api/api_lib/models/PlayerDetails.java
@@ -24,7 +24,7 @@ public class PlayerDetails {
                 .map(rsItem -> new IntPair(rsItem.getID(), rsItem.getStack())).collect(Collectors.toList());
 
         List<IntPair> settings = Stream.of(10, 11, 17, 32, 63, 68, 71, 101, 111, 116, 131, 144, 145, 150, 165, 176,
-            179, 273, 299, 302, 307, 314, 335, 347, 351, 365, 371, 387, 399, 425, 437, 440, 482, 622, 655, 671, 705, 794, 810,
+            179, 212, 273, 299, 302, 307, 314, 335, 347, 351, 365, 371, 387, 399, 425, 437, 440, 482, 622, 655, 671, 705, 794, 810,
             869, 896, 964, 970, 1630, 1671, 1672)
                                        .map(value -> new IntPair(value, Game.getSetting(value))).distinct().collect(Collectors.toList());
 
@@ -46,6 +46,7 @@ public class PlayerDetails {
             5089,
             5090,
             5810,
+            6104,
             9016
         })
                 .mapToObj(value -> new IntPair(value, RSVarBit.get(value).getValue())).distinct().collect(
