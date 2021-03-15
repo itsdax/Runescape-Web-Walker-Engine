b26436838ee409441be09e312a986fece327f285
JaccaK <79811354+JaccaK@users.noreply.github.com>
2021-03-15 03:45:56 -0700

    Change from abstract to a functional interface
    Allows people using the API to use a lambda expression to set the credentials. It should be a safe change.
    
    Example:
    DaxWalker.setCredentials(() -> new DaxCredentials("YOUR-PUBLIC-KEY", "YOUR-SECRET-KEY"));

--- scripts/dax_api/api_lib/models/DaxCredentialsProvider.java
+++ scripts/dax_api/api_lib/models/DaxCredentialsProvider.java
@@ -1,7 +1,6 @@
 package scripts.dax_api.api_lib.models;
 
-public abstract class DaxCredentialsProvider {
-
-    public abstract DaxCredentials getDaxCredentials();
-
+@FunctionalInterface
+public interface DaxCredentialsProvider {
+    DaxCredentials getDaxCredentials();
 }
