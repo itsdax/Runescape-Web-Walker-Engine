991a65e63c0939cc5d56e46651ba74094e1f7fe8
FALSkills <crlange@mail.bradley.edu>
2024-10-06 11:27:09 -0500

    Add Quetzal varbits

--- src/main/java/dax/api_lib/models/PlayerDetails.java
+++ src/main/java/dax/api_lib/models/PlayerDetails.java
@@ -64,8 +64,11 @@ public class PlayerDetails {
             8122,
             9016,
             9459, //fremennik exiles
-            9632,
+            9632, //Children of the sun,
+            9649, //Twilights Promise
             9819,
+            9956, //colossal wyrm remains quetzal landing site
+            9957, //outer fortis quetzal landing site
             10150, //we've paid 1m to Andras for free boat rides to Slepe if this is 1
             12063, //below ice mountain quest
             13719, //unlocked the 93 agility blood altar shortcut by mining the other side (78 mining req)
