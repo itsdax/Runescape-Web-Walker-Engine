a9e3d74eb2efbe5496b825e015480a449d7b9f1f
Wei <weilinmei@pixelbuf.com>
2026-03-07 20:15:48 -0500

    Add seed parameters to models

--- src/main/java/dax/api_lib/models/BulkBankPathRequest.java
+++ src/main/java/dax/api_lib/models/BulkBankPathRequest.java
@@ -13,6 +13,26 @@ public class BulkBankPathRequest {
     @DoNotRename
     private List<BankPathRequestPair> requests;
 
+
+	// Seed to use for behavior randomization on paths. Can be anything as long as it's consistent per account.
+	// Only the first 20 characters are used. Alphanumeric only. Do NOT use a new random string each time.
+	// This parameter prevents accounts from following similar paths at scale.
+	// Note that this feature is still experimental, and may produce suboptimal pathing.
+	// Leaving empty or null will disable profiling.
+	@DoNotRename
+	private String seed;
+
+	// Actual player location that this bulk path request is being generated for.
+    @DoNotRename
+    private Point3D seedLocation;
+
+    public BulkBankPathRequest(PlayerDetails player, List<BankPathRequestPair> requests, String seed, Point3D seedLocation) {
+        this.player = player;
+        this.requests = requests;
+        this.seed = seed;
+        this.seedLocation = seedLocation;
+    }
+
     public BulkBankPathRequest(PlayerDetails player, List<BankPathRequestPair> requests) {
         this.player = player;
         this.requests = requests;
@@ -24,5 +44,13 @@ public class BulkBankPathRequest {
 
     public List<BankPathRequestPair> getRequests() {
         return requests;
+    }
+
+    public String getSeed() {
+        return seed;
+    }
+
+    public Point3D getSeedLocation() {
+        return seedLocation;
     }
 }
--- src/main/java/dax/api_lib/models/BulkPathRequest.java
+++ src/main/java/dax/api_lib/models/BulkPathRequest.java
@@ -11,6 +11,26 @@ public class BulkPathRequest {
 	@DoNotRename
 	private List<PathRequestPair> requests;
 
+
+	// Seed to use for behavior randomization on paths. Can be anything as long as it's consistent per account.
+	// Only the first 20 characters are used. Alphanumeric only. Do NOT use a new random string each time.
+	// This parameter prevents accounts from following similar paths at scale.
+	// Note that this feature is still experimental, and may produce suboptimal pathing.
+	// Leaving empty or null will disable profiling.
+	@DoNotRename
+	private String seed;
+
+	// Actual player location that this bulk path request is being generated for.
+    @DoNotRename
+    private Point3D seedLocation;
+
+	public BulkPathRequest(PlayerDetails player, List<PathRequestPair> requests, String seed, Point3D seedLocation) {
+		this.player = player;
+		this.requests = requests;
+		this.seed = seed;
+		this.seedLocation = seedLocation;
+	}
+
 	public BulkPathRequest(PlayerDetails player, List<PathRequestPair> requests) {
 		this.player = player;
 		this.requests = requests;
@@ -22,5 +42,13 @@ public class BulkPathRequest {
 
 	public List<PathRequestPair> getRequests() {
 		return requests;
+	}
+
+	public String getSeed() {
+		return seed;
+	}
+
+	public Point3D getSeedLocation() {
+		return seedLocation;
 	}
 }
